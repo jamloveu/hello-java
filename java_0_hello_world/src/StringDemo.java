1bb0af3f89eb4228d2b2e0e09bcbc745c34423d9
dengjian <jamis404@163.com>
2023-02-21 13:35:19 +0800

    初识字符串

---
+++ java_0_hello_world/src/StringDemo.java
@@ -0,0 +1,17 @@
+/**
+ * @author jam
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+        // 一、创建字符串
+        // 1. 双引号
+        String s1 = "Hello world";
+
+        System.out.println("s1: " + s1);
+
+        // 2. 构造器
+        String s2 = new String("Hello jam");
+
+        System.out.println("s2: " + s2);
+    }
+}
